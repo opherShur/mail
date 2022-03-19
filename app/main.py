ff21fb0df06f8110f4dfeb0ec61ecea6ef39350f
shuropher <shuropher@gmail.com>
2022-03-19 21:15:51 +0200

    Create base endpoints and add uvicorn.run for debugging in main

--- app/main.py
+++ app/main.py
@@ -5,3 +5,17 @@ from letter import Letter
 from letter_file_utilities import write_letter_to_file, read_letter_file
 
 app = FastAPI()
+
+
+@app.get('/read_letter/{letter_id}')
+def get_letter(letter_id: str):
+    return read_letter_file(letter_id)
+
+
+@app.post('/write_letter')
+def write_letter(letter: Letter):
+    write_letter_to_file(letter)
+
+
+if __name__ == '__main__':
+    uvicorn.run(app, host="localhost", port=8000)
